7833e93abc415347c43d352c51236c6d69d05b3f
Harish K S <ksharish72@gmail.com>
2018-11-09 07:35:40 +0000

    Updates

---
+++ precache-manifest.0b8bbdcefda1dee8ad6c5b6ed0d9fe35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "36cd101951d0ba2b9486",
+    "url": "/BadmintonAcademy/static/js/runtime~main.36cd1019.js"
+  },
+  {
+    "revision": "841d3d4e2e010868bfe2",
+    "url": "/BadmintonAcademy/static/js/main.841d3d4e.chunk.js"
+  },
+  {
+    "revision": "f272308c05b0497f3cdc",
+    "url": "/BadmintonAcademy/static/js/1.f272308c.chunk.js"
+  },
+  {
+    "revision": "841d3d4e2e010868bfe2",
+    "url": "/BadmintonAcademy/static/css/main.0d946480.chunk.css"
+  },
+  {
+    "revision": "f272308c05b0497f3cdc",
+    "url": "/BadmintonAcademy/static/css/1.5387f9ad.chunk.css"
+  },
+  {
+    "revision": "28813b03b620347ff1660591bba6dbba",
+    "url": "/BadmintonAcademy/index.html"
+  }
+];
