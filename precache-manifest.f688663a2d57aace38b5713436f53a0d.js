1fd7140a422649b28a6e9c71d46b146b0d117d95
Harish K S <ksharish72@gmail.com>
2018-12-22 15:11:27 +0000

    Updates

---
+++ precache-manifest.f688663a2d57aace38b5713436f53a0d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a99fcf6dfe4d3f009807963906531703",
+    "url": "/BadmintonAcademy/static/media/juice.a99fcf6d.png"
+  },
+  {
+    "revision": "a979111eba168c5b721fea61e19fd1f8",
+    "url": "/BadmintonAcademy/static/media/headerlogo.a979111e.png"
+  },
+  {
+    "revision": "36cd101951d0ba2b9486",
+    "url": "/BadmintonAcademy/static/js/runtime~main.36cd1019.js"
+  },
+  {
+    "revision": "8d5facf6e1b6923120a2",
+    "url": "/BadmintonAcademy/static/js/main.8d5facf6.chunk.js"
+  },
+  {
+    "revision": "63d6a1ebb63060edd644",
+    "url": "/BadmintonAcademy/static/js/1.63d6a1eb.chunk.js"
+  },
+  {
+    "revision": "8d5facf6e1b6923120a2",
+    "url": "/BadmintonAcademy/static/css/main.d5f00090.chunk.css"
+  },
+  {
+    "revision": "63d6a1ebb63060edd644",
+    "url": "/BadmintonAcademy/static/css/1.7f344a8f.chunk.css"
+  },
+  {
+    "revision": "05958d9d44358396bb30137d0c152e76",
+    "url": "/BadmintonAcademy/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/BadmintonAcademy/precache-manifest.4dc25738a404ad6c2f8c550191c20874.js"
+  "/BadmintonAcademy/precache-manifest.f688663a2d57aace38b5713436f53a0d.js"
 );
 
 workbox.clientsClaim();
